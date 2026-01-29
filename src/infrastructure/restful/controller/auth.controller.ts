66408909b0cc105a6c536414860fb73e2a8df038
DatNguyenChanh <dat.nguyen-externe@soget.fr>
2026-01-29 15:56:25 +0700

    fix: wrong error try catch

--- src/infrastructure/restful/controller/auth.controller.ts
+++ src/infrastructure/restful/controller/auth.controller.ts
@@ -4,35 +4,33 @@ import { globalAuthService } from '../../factory/auth.factory';
 import { LoginResponse } from '../../../dtos/login.dto';
 
 export async function validateReviewerController(req: Request, res: Response) {
+  const personalAccessToken = req.header('x-azure-personalAccessToken');
+  if (!personalAccessToken) {
+    return res
+      .status(400)
+      .json({ message: 'missing personal access token header' });
+  }
+  const { organization, project, repositoryName } = req.body;
+
+  if (!personalAccessToken || !organization || !project || !repositoryName) {
+    return res.status(400).json({ message: 'missing parameters' });
+  }
+
   try {
-    const personalAccessToken = req.header('x-azure-personalAccessToken');
-    if (!personalAccessToken) {
-      return res
-        .status(400)
-        .json({ message: 'missing personal access token header' });
-    }
-    const { organization, project, repositoryName } = req.body;
-
-    if (!personalAccessToken || !organization || !project || !repositoryName) {
-      return res.status(400).json({ message: 'missing parameters' });
-    }
-
-    try {
-      const result: LoginResponse =
-        await globalAuthService.validateReviewerAccess({
-          organization,
-          project,
-          repositoryName,
-          personalAccessToken,
-        });
-      return res.status(200).json(result);
-    } catch (error) {
+    const result: LoginResponse =
+      await globalAuthService.validateReviewerAccess({
+        organization,
+        project,
+        repositoryName,
+        personalAccessToken,
+      });
+    if (!result) {
       return res
         .status(403)
         .json({ message: 'user is not a required reviewer' });
     }
-  } catch (e: any) {
-    console.log(e);
+    return res.status(200).json(result);
+  } catch (error) {
     return res.status(500).json({ message: 'Internal Server Error' });
   }
 }
